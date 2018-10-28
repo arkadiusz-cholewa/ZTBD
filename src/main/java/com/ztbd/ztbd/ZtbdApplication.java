1669492799fbe77010e31c98f0a51a5b39f43be0
arek-cholewa <arkadiusz.cholewaa@gmail.com>
2018-10-28 21:55:41 +0100

    new project setup

---
+++ src/main/java/com/ztbd/ztbd/ZtbdApplication.java
@@ -0,0 +1,12 @@
+package com.ztbd.ztbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtbdApplication.class, args);
+	}
+}
