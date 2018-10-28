bd61eba19a4b9bddbfdfa90d0a5c8ab5475f3540
arek-cholewa <arkadiusz.cholewaa@gmail.com>
2018-10-28 22:00:47 +0100

    new project setup 3

--- src/main/java/com/ztbd/ZtbdApplication.java
+++ src/main/java/com/ztbd/ZtbdApplication.java
@@ -1,4 +1,4 @@
-package com.ztbd.ztbd;
+package com.ztbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ztbd/ZtbdApplicationTests.java
+++ src/test/java/com/ztbd/ZtbdApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ztbd.ztbd;
+package com.ztbd;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
