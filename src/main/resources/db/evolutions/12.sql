39d0c804da0383daa12259fc0eca8d265cebac86
arkadiuszch <arkadiusz.cholewaa@gmail.com>
2019-01-15 17:25:18 +0100

    transkaction added

--- src/main/resources/db/evolutions/12.sql
+++ src/main/resources/db/evolutions/12.sql
@@ -4,5 +4,4 @@
 BEGIN WORK;
 	UPDATE loyalty_account SET balance = balance - 100.00 WHERE user_id = 1;
 	UPDATE loyalty_account SET balance = balance + 100.00 WHERE user_id = 2;
-	select * from loyalty_account where balance>5000 for update;
 COMMIT WORK;
